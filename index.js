06ce7e0b09ecb7514161c3c4708d1a5ca5f34c85
Anikesh Dhanokar <anikesh13.linuxbean@gmail.com>
2019-01-25 13:42:08 +0530

    dfsf

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
-var express = require('express');
-var app = express();
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-var connect = require('./src/connection/connect');
-var route = require('./src/router/route');
-var upload = require('./src/router/upload');
-var mail = require('./src/router/mail');
-var jwt = require('./src/router/jwt');
+// var express = require('express');
+// var app = express();
+// const mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// var connect = require('./src/connection/connect');
+// var route = require('./src/router/route');
+// var upload = require('./src/router/upload');
+// var mail = require('./src/router/mail');
+// var jwt = require('./src/router/jwt');
 
-mongoose.connect(connect.url,{ useNewUrlParser: true })
-    .then(() => {
-        console.log("Connected to Database");
-    }).catch(err => {
-        console.log(err);
-        process.exit();
-    });
+// mongoose.connect(connect.url,{ useNewUrlParser: true })
+//     .then(() => {
+//         console.log("Connected to Database");
+//     }).catch(err => {
+//         console.log(err);
+//         process.exit();
+//     });
 
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "*"
-    );
-    if (req.method == 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-        return res.status(200).json({});
-    }
-    next();
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "*"
+//     );
+//     if (req.method == 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
+
+// app.use('/route', function (req, res, next) {
+//     console.log("A request for things received at " + Date.now());
+//     next();
+// });
+
+// app.use('/route', route);
+// app.use(express.static('./src/uploads'));
+// app.use('/upload', upload);
+// app.use('/mail', mail);
+// app.use('/jwt', jwt);
+
+// app.get('**', function (req, res) {
+//     res.send('Sorry, this is an invalid URL...');
+// });
+
+// app.listen(3000); 
+
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
 });
-
-app.use('/route', function (req, res, next) {
-    console.log("A request for things received at " + Date.now());
-    next();
+server.listen(port,() => {
+console.log(`Server running at port `+port);
 });
-
-app.use('/route', route);
-app.use(express.static('./src/uploads'));
-app.use('/upload', upload);
-app.use('/mail', mail);
-app.use('/jwt', jwt);
-
-app.get('**', function (req, res) {
-    res.send('Sorry, this is an invalid URL...');
-});
-
-app.listen(3000); 
